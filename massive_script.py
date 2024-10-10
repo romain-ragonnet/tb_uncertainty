e7505ac149d1b9db9c86a74f9861cb81ce9dda5e
Romain Ragonnet <romain.ragonnet@monash.edu>
2024-10-10 14:44:00 +1100

    Set up script to run remotely

---
+++ massive_script.py
@@ -0,0 +1,91 @@
+import utils as ut
+from pathlib import Path
+import multiprocessing as mp
+from time import time
+import sys
+import yaml
+
+ANALYSIS_NAME = "test"
+CONFIG_MODE = "TEST"  # "TEST" or "FULL"
+
+parent_output_folder = Path.cwd() / "outputs"
+
+ANALYSIS_CONFIG = {
+    "TEST": {
+        'opti_budget': 100,
+
+        'mcmc_chains': 4,
+        'mcmc_cores': 4,
+        'mcmc_tune': 100,
+        'mcmc_samples': 100,
+
+        'full_runs_burnin': 50,
+        'full_runs_samples': 100,
+    },
+    "FULL": {
+        'opti_budget': 10000,
+
+        'mcmc_chains': 4,
+        'mcmc_cores': 4,
+        'mcmc_tune': 1000,
+        'mcmc_samples': 5000,
+
+        'full_runs_burnin': 2000,
+        'full_runs_samples': 1000,
+    }
+}
+
+model_config = {
+    "start_time": 1850,
+    "end_time": 2050,
+    "population": 1.e6,
+    "seed": 100,   
+    "intervention_time": 2025,
+}
+
+intervention_params = {
+    "transmission_reduction": {
+        "rel_reduction": .20
+    },
+    "preventive_treatment": {
+        "rate": .10,
+        "efficacy": .8
+    },
+    "faster_detection": {
+        "detection_rate_mutliplier": 2.
+    },
+    "improved_treatment": {
+        "negative_outcomes_rel_reduction": .50
+    }
+}
+
+fixed_params_list = [None] + [p.name for p in ut.all_priors]
+
+
+if __name__ == "__main__":
+    start_time = time()
+
+    array_task_id = int(sys.argv[2])  # specific to this particular run/analysis
+    fixed_param = fixed_params_list[array_task_id - 1]
+
+    output_root_dir = Path.home() / "sh30/users/rragonnet/outputs/"
+    array_job_id = sys.argv[1]  # common to all the tasks from this array job
+    analysis_output_dir = output_root_dir / f"{array_job_id}_{ANALYSIS_NAME}"
+    analysis_output_dir.mkdir(exist_ok=True)
+
+    # create country-specific output dir
+    param_analysis_output_dir = analysis_output_dir / fixed_param
+    param_analysis_output_dir.mkdir(exist_ok=True)
+
+    mp.set_start_method("spawn")  # previously "forkserver"
+
+    # Load MLE params
+    mle_path = Path.cwd() / "data" / "mle_params.yml"
+    with open(mle_path, 'r') as file:
+        mle_params = yaml.safe_load(file)
+
+    fixed_param = None
+    analysis_name = 'test' 
+    ut.run_analysis(fixed_param, mle_params, ANALYSIS_CONFIG[CONFIG_MODE], model_config, intervention_params, param_analysis_output_dir)
+
+    print(f"Finished in {time() - start_time} seconds", flush=True)
